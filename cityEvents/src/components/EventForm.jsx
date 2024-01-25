011de63f5b802ce8910e2c063da1023022ea66e4
Prabeshika <prabeshika14@gmail.com>
2024-01-25 17:48:56 +0100

    Event Form error resolve

--- cityEvents/src/components/EventForm.jsx
+++ cityEvents/src/components/EventForm.jsx
@@ -1,8 +1,8 @@
 import { useState, useContext } from "react";
-import { Form, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthContext";
 
-const EventForm = () => {
+const EventForm = ({ isLogin = true }) => {
   const [title, setTitle] = useState("");
   const [organiser, setOrganiser] = useState("");
   const [date, setDate] = useState("");
@@ -10,6 +10,18 @@ const EventForm = () => {
   const [price, setPrice] = useState("");
   const [description, setDescription] = useState("");
   const [image, setImage] = useState("");
+
+  const { saveToken } = useContext(AuthContext);
+
+  const handleTitle = (event) => setTitle(event.target.value);
+  const handleOrganiser = (event) => setOrganiser(event.target.value);
+  const handleDate = (event) => setDate(event.target.value);
+  const handleLocation = (event) => setLocation(event.target.value);
+  const handlePrice = (event) => setPrice(event.target.value);
+  const handleDescription = (event) => setDescription(event.target.value);
+  const handleImage = (event) => setImage(event.target.value);
+
+  const navigate = useNavigate();
   // HOW TO UPLOAD THE IMG FROM LOCAL DISC
 
   const handleSubmit = async (event) => {
@@ -24,7 +36,13 @@ const EventForm = () => {
       image,
     };
     try {
-      const response = await fetchWithToken("/events", "POST", eventToCreate);
+      const response = await fetch("/events", "POST", eventToCreate);
+      `${import.meta.env.VITE_API_URL}/auth/${isLogin ? "login" : "signup"}`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(eventToCreate),
+        };
       if (response.status === 201) {
         const event = await response.json();
         console.log(event);
@@ -40,37 +58,32 @@ const EventForm = () => {
   return (
     <form onSubmit={handleSubmit}>
       <label>
-        {" "}
         Title:
         <input type="text" value={title} onChange={handleTitle} required />
       </label>
       <label>
         Organiser:
-        <input type="text" value={organiser} onChange={handleOrganiser} />{" "}
+        <input type="text" value={organiser} onChange={handleOrganiser} />
       </label>
       <label>
         Date:
-        <input type="date" value={date} onChange={handleDate} />{" "}
+        <input type="date" value={date} onChange={handleDate} />
       </label>
       <label>
         Location:
-        <input type="text" value={location} onChange={handleLocation} />{" "}
+        <input type="text" value={location} onChange={handleLocation} />
       </label>
       <label>
         Price:
-        <input type="number" value={price} onChange={handlePrice} />{" "}
+        <input type="number" value={price} onChange={handlePrice} />
       </label>
       <label>
         Description:
-        <input
-          type="text"
-          value={description}
-          onChange={handleDescription}
-        />{" "}
+        <input type="text" value={description} onChange={handleDescription} />
       </label>
       <label>
         Image:
-        <input type="text" value={image} onChange={handleImage} />{" "}
+        <input type="text" value={image} onChange={handleImage} />
       </label>
 
       <button type="submit">{isLogin ? "Login" : "Signup"}</button>
