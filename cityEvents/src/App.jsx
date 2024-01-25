30ffa46af35ca882e9e86a6a87fc15fec7e143fa
Prabeshika <prabeshika14@gmail.com>
2024-01-25 21:19:30 +0100

    added error page

--- cityEvents/src/App.jsx
+++ cityEvents/src/App.jsx
@@ -2,6 +2,7 @@ import EventForm from "./components/EventForm";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import { Route, Routes } from "react-router-dom";
+import Error404Page from "./pages/Error404Page";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/" element={<EventForm />} />
+        <Route path="*" element={<Error404Page />} />
       </Routes>
     </>
   );
---
+++ cityEvents/src/pages/Error404Page.jsx
@@ -0,0 +1,11 @@
+import image from "../assets/error-404-.avif";
+
+const Error404Page = () => {
+  return (
+    <div>
+      <img src={image} alt="No page found" />
+    </div>
+  );
+};
+
+export default Error404Page;
