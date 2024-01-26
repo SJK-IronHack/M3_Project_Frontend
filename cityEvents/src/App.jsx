7ab5ff6eeb46147205602f1a42cdb7ceec96d215
mirJakub <slawomir.krzyzak@gmail.com>
2024-01-26 17:47:25 +0100

    user profile, eveent listing

--- cityEvents/src/App.jsx
+++ cityEvents/src/App.jsx
@@ -5,17 +5,18 @@ import Error404Page from "./pages/Error404Page";
 import AddEventPage from "./pages/AddEventPage";
 import UserProfilePage from "./pages/UserProfilePage";
 import Navbar from "./components/navbar";
+import EventListing from "./components/EventListing";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <EventListing/>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/addevent" element={<AddEventPage />} />
         <Route path="/addevent" element={<UserProfilePage />} />
-
         <Route path="*" element={<Error404Page />} />
       </Routes>
     </div>
