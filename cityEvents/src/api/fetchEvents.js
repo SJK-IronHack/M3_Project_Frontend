137537d2dfda7536a2e9dd1c469527a2fa820520
Janos Sitku <sitku.janos86@gmail.com>
2024-01-29 14:32:56 +0100

    version from 2:30

--- cityEvents/src/api/fetchEvents.js
+++ cityEvents/src/api/fetchEvents.js
@@ -6,8 +6,8 @@ export const fetchEvents = () => {
     mode: "cors",
     headers: {
       "Content-Type": "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NWIyNzgwNzE2MGIwZGViZGFiZjNhOWUiLCJpYXQiOjE3MDY1MTQ2MTAsImV4cCI6MTcwNjUzNjIxMH0.82uXPw-ErL2P1_udlnu_pQihAOQeq9P2SNUEQ1C6Tag",
+      // Authorization:
+      //   "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2NWIyNzgwNzE2MGIwZGViZGFiZjNhOWUiLCJpYXQiOjE3MDY1MTQ2MTAsImV4cCI6MTcwNjUzNjIxMH0.82uXPw-ErL2P1_udlnu_pQihAOQeq9P2SNUEQ1C6Tag",
     },
   }).then((response) => response.json());
 };
