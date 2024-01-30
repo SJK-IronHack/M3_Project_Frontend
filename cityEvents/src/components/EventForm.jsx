9fa371af24fa28768280548e70e1092b027d63d7
mirJakub <slawomir.krzyzak@gmail.com>
2024-01-30 13:18:51 +0100

    event form mantine

--- cityEvents/src/components/EventForm.jsx
+++ cityEvents/src/components/EventForm.jsx
@@ -1,75 +1,74 @@
-import { Box, TextField } from "@mui/material";
+import { Col, Input, Textarea, DatePicker, NumberInput } from '@mantine/core';
 
 const EventForm = ({ handleChange, event }) => {
   return (
-    <Box>
-      <TextField
-        id="title"
-        name="title"
-        label="Title"
-        variant="standard"
-        value={event.title}
-        onChange={(event) => handleChange("title", event.target.value)}
-        fullWidth
-        required
-      />
-      <TextField
-        id="organiser"
-        name="organiser"
-        label="Organiser"
-        variant="standard"
-        value={event.organiser}
-        onChange={(event) => handleChange("organiser", event.target.value)}
-        fullWidth
-      />
-      <TextField
-        id="date"
-        name="date"
-        label="Date"
-        variant="standard"
-        type="date"
-        value={event.date}
-        onChange={(event) => handleChange("date", event.target.value)}
-        fullWidth
-      />
-      <TextField
-        id="location"
-        name="location"
-        label="Location"
-        variant="standard"
-        value={event.location}
-        onChange={(event) => handleChange("location", event.target.value)}
-        fullWidth
-      />
-      <TextField
-        id="price"
-        name="price"
-        label="Price"
-        variant="standard"
-        type="number"
-        value={event.price}
-        onChange={(event) => handleChange("price", event.target.value)}
-        fullWidth
-      />
-      <TextField
-        id="description"
-        name="description"
-        label="Description"
-        variant="standard"
-        value={event.description}
-        onChange={(event) => handleChange("description", event.target.value)}
-        fullWidth
-      />
-      <TextField
-        id="image"
-        name="image"
-        label="Image"
-        variant="standard"
-        value={event.image}
-        onChange={(event) => handleChange("image", event.target.value)}
-        fullWidth
-      />
-    </Box>
+    <>
+      <Col span={12}>
+        <Input
+          id="title"
+          name="title"
+          label="Title"
+          value={event.title}
+          onChange={(event) => handleChange('title', event.target.value)}
+          required
+        />
+      </Col>
+      <Col span={12}>
+        <Input
+          id="organiser"
+          name="organiser"
+          label="Organiser"
+          value={event.organiser}
+          onChange={(event) => handleChange('organiser', event.target.value)}
+        />
+      </Col>
+      <Col span={12}>
+        <DatePicker
+          id="date"
+          name="date"
+          label="Date"
+          value={event.date}
+          onChange={(date) => handleChange('date', date)}
+        />
+      </Col>
+      <Col span={12}>
+        <Input
+          id="location"
+          name="location"
+          label="Location"
+          value={event.location}
+          onChange={(event) => handleChange('location', event.target.value)}
+        />
+      </Col>
+      <Col span={12}>
+        <NumberInput
+          id="price"
+          name="price"
+          label="Price"
+          value={event.price}
+          onChange={(value) => handleChange('price', value)}
+        />
+      </Col>
+      <Col span={12}>
+        <Textarea
+          id="description"
+          name="description"
+          label="Description"
+          value={event.description}
+          onChange={(event) => handleChange('description', event.target.value)}
+        />
+      </Col>
+      <Col span={12}>
+        <Input
+          id="image"
+          name="image"
+          label="Image"
+          value={event.image}
+          onChange={(event) => handleChange('image', event.target.value)}
+        />
+      </Col>
+    </>
   );
 };
+
 export default EventForm;
